b57b43a67effa55c717fdbd90cd508aa6ded5b18
PT-Leprechaun <diogolessareis2001@gmail.com>
2021-04-30 12:14:16 +0100

    testing something

--- java/src/ie/Planet.java
+++ java/src/ie/Planet.java
@@ -1,19 +1,24 @@
 package ie;
 
-import processing.core.PApplet;
+import ie.Visual;
+import ie.VisualException;
 
-public class Planet extends Starfield {
+public class Planet extends Visual {
 
     Starfield starfield;
 
-    public Planet(Starfield starfield) {
-
+    public Planet(Starfield starfield) 
+    {
         this.starfield = starfield;
-
-        // println("test1");
-
     }
-    
+
+    public void setup()
+    {
+        colorMode(HSB);
+        //noCursor();
+        
+        setFrameSize(256);
+    }
 
     float radius = 200;
 
@@ -23,96 +28,151 @@ public class Planet extends Starfield {
 
     float rotRight = 0;
 
+
     public void render()
     {
-        // println("test2");
-        starfield.calculateAverageAmplitude();
+        calculateAverageAmplitude();
         try
         {
-            starfield.calculateFFT();
+            calculateFFT();
         }
         catch(VisualException e)
         {
             e.printStackTrace();
         }
-        starfield.calculateFrequencyBands();
+        calculateFrequencyBands();
+        background(0);
+        noFill();
+        stroke(255);
+        stroke(map(getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
+        camera(mouseX*-1, mouseY*-1, 600, 0, 0, 0, 0, 1, 0);
+        //camera(-250, -250, -mouseX, -mouseY, 0, 0, 0, 1, 0);
+        //translate(0, 0, -250);
 
-        float[] bands = starfield.getSmoothedBands();
+        rotLeft += getAmplitude() / 2.0f;
 
-        rotLeft += starfield.getAmplitude() / 2.0f;
+        rotRight -= getAmplitude() / 7.0f;
 
-        rotRight -= starfield.getAmplitude() / 7.0f;
+        float[] bands = getSmoothedBands();
 
-        starfield.rotateY(rotLeft);
+        rotateY(rotLeft);
         for(int i = 0 ; i < bands.length ; i ++)
         {
-            float theta = PApplet.map(i, 0,bands.length, 0, TWO_PI);
+            float theta = map(i, 0, bands.length, 0, TWO_PI);
 
             //stroke(map(i, 0, bands.length, 0, 255), 255, 255);
-            starfield.stroke(0,0,255);
+            stroke(0,0,255);
             float h = bands[i];
-            float x = PApplet.sin(theta) * (radius * 1.3f);
-            float z = PApplet.cos(theta) * (radius * 1.3f);
+            float x = sin(theta) * (radius * 1.3f);
+            float z = cos(theta) * (radius * 1.3f);
 
-            starfield.pushMatrix();
-            starfield.fill(255,255,255);
-            starfield.translate(x, 0, z);
-            starfield.rotateX(theta);
-            starfield.sphere(10);
-            starfield.popMatrix();
+            pushMatrix();
+            fill(255,255,255);
+            translate(x, 0, z);
+            rotateX(theta);
+            sphere(10);
+            popMatrix();
 
-            // println("test3");
-
-            starfield.pushMatrix();
+            pushMatrix();
             //stroke(0, 0, 255);
-            starfield.colorMode(HSB);
-            starfield.stroke(PApplet.map(i, 0, bands.length, 0, 255), 255, 255);
-            starfield.noFill();
-            starfield.sphere(h / 50);
-            starfield.popMatrix();
+            colorMode(HSB);
+            stroke(map(i, 0, bands.length, 0, 255), 255, 255);
+            noFill();
+            sphere(h / 50);
+            popMatrix();
         }
-        starfield.rotateX(rotRight);
+        rotateX(rotRight);
         for(int i = 0 ; i < bands.length ; i ++)
         {
 
-            float theta = PApplet.map(i, 0, bands.length, 0, TWO_PI);
+            float theta = map(i, 0, bands.length, 0, TWO_PI);
 
-            starfield.stroke(PApplet.map(i, 0, bands.length, 0, 255), 255, 255);
+            stroke(map(i, 0, bands.length, 0, 255), 255, 255);
             //stroke(0,0,255);
             //float h = bands[i];
-            float x = PApplet.sin(theta) * (radius * 1.3f);
-            float z = PApplet.cos(theta) * (radius * 1.3f);
-            starfield.pushMatrix();
-            starfield.noFill();
+            float x = sin(theta) * (radius * 1.3f);
+            float z = cos(theta) * (radius * 1.3f);
+            pushMatrix();
+            noFill();
 
-            starfield.translate(-x , x , -z);
+            translate(-x , x , -z);
             
-            // println("test4");
 
-            starfield.sphere(10);
+            sphere(10);
 
-            starfield.popMatrix();
+            popMatrix();
         }
         for(int i = 0 ; i < bands.length ; i ++)
         {
             
-            float theta = PApplet.map(i, 0, bands.length, 0, TWO_PI);
-
-            starfield.stroke(map(i, 0, bands.length, 0, 255), 255, 255);
-            //stroke(0,0,255);
-            float x = PApplet.sin(theta) * (radius * 1.3f);
-            float z = PApplet.cos(theta) * (radius * 1.3f);
-            starfield.pushMatrix();
             
-            starfield.noFill();
+            float theta = map(i, 0, bands.length, 0, TWO_PI);
 
-            starfield.translate(x , x , -z);
-            starfield.sphere(10);
+            stroke(map(i, 0, bands.length, 0, 255), 255, 255);
+            //stroke(0,0,255);
+            float x = sin(theta) * (radius * 1.3f);
+            float z = cos(theta) * (radius * 1.3f);
+            pushMatrix();
+            
+            noFill();
 
-            // println("test5");
+            translate(x , x , -z);
+            sphere(10);
 
-            starfield.popMatrix();
+
+            popMatrix();
             
         }
+
     }
+    float angle = 0;
+
+    public Starfield getStarfield() {
+        return starfield;
+    }
+
+    public void setStarfield(Starfield starfield) {
+        this.starfield = starfield;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+
+    public float getSmoothedBoxSize() {
+        return smoothedBoxSize;
+    }
+
+    public void setSmoothedBoxSize(float smoothedBoxSize) {
+        this.smoothedBoxSize = smoothedBoxSize;
+    }
+
+    public float getRotLeft() {
+        return rotLeft;
+    }
+
+    public void setRotLeft(float rotLeft) {
+        this.rotLeft = rotLeft;
+    }
+
+    public float getRotRight() {
+        return rotRight;
+    }
+
+    public void setRotRight(float rotRight) {
+        this.rotRight = rotRight;
+    }
+
+    public float getAngle() {
+        return angle;
+    }
+
+    public void setAngle(float angle) {
+        this.angle = angle;
+    }
+
 }
